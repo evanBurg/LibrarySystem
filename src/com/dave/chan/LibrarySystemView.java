11e08cd21653899ad759b48132ffc988dfaded68
David <dave.harris818@gmail.com>
2018-07-30 14:52:46 -0400

    Baseline GUI
    beginning of david's teenie weenie VIEW

--- src/com/dave/chan/LibrarySystemView.java
+++ src/com/dave/chan/LibrarySystemView.java
@@ -2,32 +2,65 @@ package com.dave.chan;
 
 /**
  * Program Name: LibrarySystemView.java
- * Purpose: shows use of the MVC architecture. This class will represent
- *          the VIEW portion, or what the user actually sees. It will do
- *          double duty in that it will act as BOTH the input GUI and the
- *          display GUI.
- * Coder: Bill Pulling for Sec02, ADAPTED from Derek Carnas' online Youtube video
- *        on MVC.
- * Date: Jul 12, 2016
+ * Purpose: A GUI that the user will interact with in order to operate our library system. The View section of the
+ * 			MVC architecture.
+ * Coder: David Harrs & Evan Burgess
+ * Date: July 30, 2018
  */
-//three wise men
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class LibrarySystemView extends JFrame
 {
-    //CLASS WIDE SCOPE AREA
-
-    //constructor
+	//class-wide
+	JTabbedPane libraryTabbedPane;
+	JFrame libraryFrame;
+	JPanel usersPanel, booksPanel, loansPanel, retrievalPanel;
     public LibrarySystemView()
     {
-        super("Library");
         //boilerplate
+        super("Library System");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(new FlowLayout() );
-        this.setSize(500,200);
+        this.setLayout(new FlowLayout());
+        this.setSize(500,500);
         this.setLocationRelativeTo(null);
+        
+        libraryFrame = new JFrame();
+        libraryTabbedPane = new JTabbedPane();
+        libraryFrame.add(libraryTabbedPane);
+        
+        //Users Section
+        usersPanel = new JPanel();
+        
+        libraryTabbedPane.add("Users", usersPanel);
+        
+        
+        //Books Section
+        booksPanel = new JPanel();
+        
+        libraryTabbedPane.addTab("Books", booksPanel);
+        
+        //Loans Section
+        loansPanel = new JPanel();
+        
+        libraryTabbedPane.addTab("Loans", loansPanel);
+        
+        //Retrieval Section
+        retrievalPanel = new JPanel();
+        
+        libraryTabbedPane.addTab("Retrieval", retrievalPanel);
+        
+        //display it
+        libraryFrame.setVisible(true);
+        
     }//end constructor
+    
+    
+    
+    public static void main (String[] args) {
+    	new LibrarySystemView();
+    }
 
 }//end class
